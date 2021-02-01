b638d84cace8b02a867f324e90a7869942ac6d08
Edjane <edjane@gmail.com>
2021-02-01 14:11:20 +0000

    work in progress

--- Lessons/Section_01/Activity_2.py
+++ Lessons/Section_01/Activity_2.py
@@ -4,15 +4,25 @@ between three given points
 """
 
 # Initialize variables
+print('> x, y, z = 2, 3, 4')
 x, y, z = 2, 3, 4
+print('> print(x, y, z)')
+print(x, y, z)
+print()
 
 # Pythagorean Theorem in 3 dimensions
+print('> w_square = x ** 2 + y ** 2 + z ** 2')
 w_square = x ** 2 + y ** 2 + z ** 2
+print('> print(w_square)')
+print(w_square)
+print()
 
 # The square root gives the distance
+print('> w = w_square ** 0.5')
 w = w_square ** 0.5
+print('> print(w)')
 
-#Show the distance
+# show the distance
 print(w)
-
+print()
 
