f6e62afee81009f35e62d9731d6d427ac932ad8d
Edjane <edjane@gmail.com>
2021-02-01 14:14:09 +0000

    work in progress

--- Lessons/Section_01/Activity_1.py
+++ Lessons/Section_01/Activity_1.py
@@ -1,72 +1,72 @@
 # Set the variable x to 14
 x = 14
-print('> x = 14')
-print('> print(x)')
+print('>>> x = 14')
+print('>>> print(x)')
 print(x)
 print()
 
 # add 1 to the value of x
 x += 1
-print('> x += 1')
-print('> print(x)')
+print('>>> x += 1')
+print('>>> print(x)')
 print(x)
 print()
 
 # divide x by 5 and cube it
 x = (x/5) ** 2
-print('> x = x/5 ** 2')
-print('> print(x)')
+print('>>> x = x/5 ** 2')
+print('>>> print(x)')
 print(x)
 print()
 
 # playing with variable reassignments
 x = 0
-print('> x = 0')
-print('> print(x)')
+print('>>> x = 0')
+print('>>> print(x)')
 print(x)
 print()
 
 # add 6 to the value of x
-print('> print(x)')
+print('>>> print(x)')
 print(x)
-print('> x += 6')
+print('>>> x += 6')
 x += 6
-print('> print(x)')
+print('>>> print(x)')
 print(x)
 print()
 
 # subtract 2 from the value of x
-print('> print(x)')
+print('>>> print(x)')
 print(x)
-print('> x -= 2')
+print('>>> x -= 2')
 x -= 2
-print('> print(x)')
+print('>>> print(x)')
 print(x)
 print()
 
 # multiply the value of x by 2
-print('> print(x)')
+print('>>> print(x)')
 print(x)
-print('> x *= 2')
+print('>>> x *= 2')
 x *= 2
-print('> print(x)')
+print('>>> print(x)')
 print(x)
 print()
 
 # divide the value of x by 4
-print('> print(x)')
+print('>>> print(x)')
 print(x)
-print('> x //= 4')
+print('>>> x //= 4')
 x //= 4
-print('> print(x)')
+print('>>> print(x)')
 print(x)
 print()
 
 # raise the value of x to the power of 8
-print('> print(x)')
+print('>>> print(x)')
 print(x)
-print('> x **= 8')
+print('>>> x **= 8')
 x **= 8
-print('> print(x)')
+print('>>> print(x)')
 print(x)
 print()
--- Lessons/Section_01/Activity_2.py
+++ Lessons/Section_01/Activity_2.py
@@ -4,23 +4,23 @@ between three given points
 """
 
 # Initialize variables
-print('> x, y, z = 2, 3, 4')
+print('>>> x, y, z = 2, 3, 4')
 x, y, z = 2, 3, 4
-print('> print(x, y, z)')
+print('>>> print(x, y, z)')
 print(x, y, z)
 print()
 
 # Pythagorean Theorem in 3 dimensions
-print('> w_square = x ** 2 + y ** 2 + z ** 2')
+print('>>> w_square = x ** 2 + y ** 2 + z ** 2')
 w_square = x ** 2 + y ** 2 + z ** 2
-print('> print(w_square)')
+print('>>> print(w_square)')
 print(w_square)
 print()
 
 # The square root gives the distance
-print('> w = w_square ** 0.5')
+print('>>> w = w_square ** 0.5')
 w = w_square ** 0.5
-print('> print(w)')
+print('>>> print(w)')
 
 # show the distance
 print(w)
