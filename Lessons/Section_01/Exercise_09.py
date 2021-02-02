1186b139630372ad0d1b0cd5f7f68d205a6270b3
Edjane <edjane@gmail.com>
2021-02-02 00:32:12 +0000

    section 1 - exercise 10

---
+++ Lessons/Section_01/Exercise_09.py
@@ -0,0 +1,10 @@
+print(">>> spanish_greeting = 'Hola'")
+spanish_greeting = 'Hola'
+print(">>> print('spanish_greeting + 'Senor.')")
+print(spanish_greeting + 'Senor.')
+print(">>> print('spanish_greeting + 'Senor.')")
+print(spanish_greeting + ' Senor.')
+print(">>> greeting = 'Hello'")
+greeting = 'Hello'
+print(">>> print(greeting * 5)")
+print(greeting * 5)
---
+++ Lessons/Section_01/Exercise_10.py
@@ -0,0 +1,8 @@
+# String interpolation: command separators and format
+
+# Comma separators
+# Similart to concactenantion, but adds space between values
+print("italian_greeting = 'Ciao'")
+italian_greeting = 'Ciao'
+
+print('Should we greet people with', italian_greeting, 'in North Beach?')
