c5a58cb42f5ae4bb789eae1f94ff5b9af673c454
Edjane <edjane@gmail.com>
2021-02-02 14:10:01 +0000

    continued lessons

---
+++ Lessons/Section_01/Exercise_14.py
@@ -0,0 +1,43 @@
+# Boolean operators
+print(">>> over_18, over_21 = True, False")
+over_18, over_21 = True, False
+
+print(">>> over_18 and over_21")
+print(over_18 and over_21)
+
+print(">>> over_18 or over_21")
+print(over_18 or over_21)
+
+print(">>> not over_18")
+print(not over_18)
+
+print(">>> not over_21 or (over_21 or over_18)")
+print(not over_21 or (over_21 or over_18))
+
+# comparison operators
+print(">>> age = 20")
+age = 20
+
+print(">>> age < 13")
+print(age < 13)
+
+print(">>> age >= 20 and age <= 21")
+print(age >= 20 and age <=21)
+
+print(">>> age != 21")
+print(age != 21)
+
+print(">>> age == 19")
+print(age == 19)
+
+print(">>> 6 = 6.0")
+print(6 == 6.0)
+
+print(">>> 6 = '6'")
+print(6 == '6')
+
+print(">>> (age >= 20 and age < 30) or (age >= 30 and age < 40)")
+print((age >= 20 and age < 30) or (age >= 30 and age < 40))
+
+print(">>> (20 <= age < 30) or (30 <= age < 40)")
+print((20 <= age < 30) or (30 <= age < 40))
